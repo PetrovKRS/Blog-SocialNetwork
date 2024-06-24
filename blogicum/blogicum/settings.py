7e0b79daa55e9f74d80e155a1649083ae9a15193
petrovkrs <petrovkrs@yandex.ru>
2024-06-24 11:35:42 +0300

    README

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static_dev',
@@ -139,8 +141,6 @@ INTERNAL_IPS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Имя view-функции, обрабатывающей ошибку 403
 CSRF_FAILURE_VIEW = 'pages.views.csrf_err'
